1379224e12e459bb287e34aff5df314e499cf8be
shaila19 <shailafareed@gmail.com>
2022-12-19 15:04:29 +0100

    Turbine Expansion simple code

---
+++ calculation/2022-11-28 - Shaila Folder/Off-Design.py
@@ -0,0 +1,127 @@
+# %%------------   IMPORT MODULES                     -----------------------------------------------------------> #
+from main_code.support.abstract_plant_thermo_point import PlantThermoPoint
+from main_code.simplified_BHE.simplified_BHE import SimplifiedBHE
+
+# %%------------   BH input condition                   -----------------------------------------------------------> #
+
+#let's assume a 0.7Mpa of pump pressure rise
+delta_pump_p=0.7
+
+#Pump input condition
+T_amb = 15 # [°C]
+dT_appr = 7  # [°C]
+T_c_out=T_amb+dT_appr       #condenser Temperature
+
+
+CO2_input = PlantThermoPoint(["CarbonDioxide"], [1])
+CO2_input.set_variable("T", T_c_out)
+
+
+        if T_c_out < 30:
+
+            CO2_input.set_variable("Q", 0.)
+            p_c = CO2_input.get_variable("P")  # condenser pressure or turbine outlet pressure
+
+        else:
+
+            CO2_input.set_variable("rho", 700)
+            p_c = CO2_input.get_variable("P")
+        p_c = CO2_input.get_variable("P")
+        print(p_c)
+
+
+
+
+CO2_input.set_variable("T", T_c_out)
+p_in_BH=p_c+delta_pump_p
+CO2_input.set_variable("P", p_in_BH)
+h_in_BH = CO2_input.get_variable("h")
+
+print("BH input pressure", p_in_BH)
+print(CO2_input.get_unit("D"))
+
+
+
+
+
+# %%------------     BH  Output condition               -----------------------------------------------------------> #
+
+dz_well = 1500  # [m] Depth of the reservoir
+T_rock = 125    # [°C] Temperature of the reservoir
+
+bhe_inside = SimplifiedBHE(
+
+    input_thermo_point=CO2_input,
+    dz_well=dz_well, T_rocks=T_rock, use_rk=True
+
+)
+
+
+bhe_inside.update()
+print(bhe_inside)
+
+output_condition = bhe_inside.points[-1]
+p_out_BH = output_condition.get_variable("P")
+t_out_BH = output_condition.get_variable("T")
+h_out_BH = output_condition.get_variable("h")
+s_out_BH = output_condition.get_variable("s")
+unit_p = output_condition.get_unit("P")
+print(output_condition)
+
+# %%------------ Turbine Expansion                      -----------------------------------------------------------> #
+
+P_t_out=p_c
+
+Turbine_input_condition=bhe_inside.points[-1]
+p_t_in = Turbine_input_condition.get_variable("P")
+t_t_in = Turbine_input_condition.get_variable("T")
+h_t_in = Turbine_input_condition.get_variable("h")
+s_t_in = Turbine_input_condition.get_variable("s")
+
+Turbine_output_condition_iso = PlantThermoPoint(["CarbonDioxide"], [1])
+s_t_out=Turbine_output_condition_iso.set_variable("s",s_t_in)
+Turbine_output_condition_iso.set_variable("p", P_t_out)
+
+h_out_iso = Turbine_output_condition_iso.get_variable("h")
+t_t_out_iso= Turbine_output_condition_iso.get_variable("t")
+print("turbine_out_t_iso:",t_t_out_iso)
+
+eta=0.8
+
+h_t_out=h_t_in-(eta*(h_t_in-h_out_iso))
+print("h_t_out",h_t_out)
+print("h_t_in",h_t_in)
+delta_h_t=h_t_in-h_t_out
+P_t= 1000  # kW Fixing as desiered power output
+m_dot= P_t/delta_h_t   #kg/s
+print("mass flow rate:Design",m_dot,"kg/s")
+
+Turbine_output_condition_real = PlantThermoPoint(["CarbonDioxide"], [1])
+Turbine_output_condition_real.set_variable("h",h_t_out)
+Turbine_output_condition_real.set_variable("P", P_t_out)
+
+s_t_out= Turbine_output_condition_real.get_variable("s")
+t_t_out= Turbine_output_condition_real.get_variable("t")
+print("turbine outlet temperature", t_t_out, "c")
+beta=p_t_in/P_t_out
+print("beta:",beta)
+
+# %%------------ condenser                      -----------------------------------------------------------> #
+T_c_in=t_t_out
+condenser_output_condition= PlantThermoPoint(["CarbonDioxide"], [1])
+condenser_output_condition.set_variable("Q",0)
+condenser_output_condition.set_variable("P", p_c)
+
+h_c_out= condenser_output_condition.get_variable("h")
+
+print("h_c_out:",h_c_out)
+Q_out=h_t_out-h_c_out
+print("Q_out:",Q_out)
+
+# %%------------   cycle efficiency neglecting pump wor ----------------------------------------------------------->
+eta_cycle=100*(h_t_in-h_t_out)/(h_out_BH-h_in_BH)
+print("eta_cycle:",eta_cycle)
+
+# %%------------ Off design Analisys----------------------------------------------------------->
+
+n_S= 3      #Number of stages
--- calculation/2022-11-28 - Shaila Folder/first-trial.py
+++ calculation/2022-11-28 - Shaila Folder/first-trial.py
@@ -4,7 +4,7 @@ from main_code.simplified_BHE.simplified_BHE import SimplifiedBHE
 
 
 # %%------------   INIT INPUT CONDITION                   -----------------------------------------------------------> #
-T_amb = 23 # [°C]
+T_amb = 22 # [°C]
 dT_appr = 7  # [°C]
 Y_id = 5213221
 
@@ -27,7 +27,7 @@ CO2_input.set_variable("P", p_in*1.0001)
 print("BH input pressure", p_in)
 print(CO2_input.get_unit("D"))
 
-# %%------------   INIT BHE WELL                          -----------------------------------------------------------> #
+ # %%------------   INIT BHE WELL                          -----------------------------------------------------------> #
 dz_well = 1500  # [m] Depth of the reservoir
 T_rock = 125    # [°C] Temperature of the reservoir
 
@@ -48,11 +48,12 @@ t_out = output_condition.get_variable("T")
 h_out = output_condition.get_variable("h")
 s_out = output_condition.get_variable("s")
 unit_p = output_condition.get_unit("P")
-
+print(output_condition)
 
 # %%------------   TURBINE EXPANSION                      -----------------------------------------------------------> #
-
-P_t_out=p_in
+delta_P_pump=0.7  #pump pressure rise
+P_t_out=p_in-delta_P_pump
+print(P_t_out)
 
 Turbine_input_condition=bhe_in.points[-1]
 p_t_in = Turbine_input_condition.get_variable("P")
@@ -65,49 +66,57 @@ s_t_out=Turbine_output_condition_iso.set_variable("s",s_t_in)
 Turbine_output_condition_iso.set_variable("p", P_t_out)
 
 h_out_iso = Turbine_output_condition_iso.get_variable("h")
+t_t_out_iso= Turbine_output_condition_iso.get_variable("t")
+print("t_out_t_iso:",t_t_out_iso)
 eta=0.8
 h_t_out=h_t_in-(eta*(h_t_in-h_out_iso))
-print(h_t_out)
-print(h_t_in)
+print("h_t_out",h_t_out)
+print("h_t_in",h_t_in)
 delta_h_t=h_t_in-h_t_out
 P_t= 1000  # kW
 m_dot= P_t/delta_h_t   #kg/s
-print("mass flow rate",m_dot)
+print("mass flow rate:Design",m_dot,"kg/s")
 
 Turbine_output_condition_real = PlantThermoPoint(["CarbonDioxide"], [1])
 Turbine_output_condition_real.set_variable("h",h_t_in)
 Turbine_output_condition_real.set_variable("P", P_t_out)
 
+s_t_out= Turbine_output_condition_real.get_variable("s")
 t_t_out= Turbine_output_condition_real.get_variable("t")
 print("turbine outlet temperature", t_t_out, "c")
-
-#
+beta=p_t_in/P_t_out
+print("beta:",beta)
 
 # %%------------   Condensser                      ----------------------------------------------------------->
 
-p_c= p_in  #condenser pressure
+p_c= p_in-delta_P_pump  #condenser pressure
 delta_t_c=t_t_out-(T_amb+dT_appr)
 condenser_output_condition= PlantThermoPoint(["CarbonDioxide"], [1])
 condenser_output_condition.set_variable("Q",0)
 condenser_output_condition.set_variable("T", T_amb + dT_appr)
 
+
+h_c_out= condenser_output_condition.get_variable("h")
 t_c_out= condenser_output_condition.get_variable("t")
-
+print(t_c_out)
+print(h_c_out)
+Q_out=h_t_out-h_c_out
+print("Q_out:",Q_out)
 # %%------------   Pump work                      ----------------------------------------------------------->
+Pump_p_in=p_c
+Pump_W=h_out-h_c_out
+print("Pump_W:",Pump_W)
 
-pump_input_condition=bhe_in.points[0]
-print(pump_input_condition)
-
-pump_output_condition=bhe_in.points[1]
-print(pump_output_condition)
+# %%------------   Cycle efficiency                      ----------------------------------------------------------->
+eta_des=0.7
 
 # %%------------   evaluate off design                     ----------------------------------------------------------->
 
 def calc_function(t_amb, opt_parm):
 
     # Set the input condition for the BHE well
-    bhe_in.update()
 
     # evaluate the turbine off_desing
+
 
     # evauate and return the power
