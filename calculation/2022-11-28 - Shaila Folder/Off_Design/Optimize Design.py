5467d4f341eba359f33b4b20a817db6a40929c20
Pietro Ungar <pietro.ungar@unifi.it>
2023-02-20 15:35:37 +0100

    tqdm explaination

--- calculation/2022-11-28 - Shaila Folder/Off_Design/Optimize Design.py
+++ calculation/2022-11-28 - Shaila Folder/Off_Design/Optimize Design.py
@@ -8,13 +8,16 @@ from numpy import linspace
 from pandas import Series,DataFrame
 import matplotlib.pyplot as plt
 import calendar
+from tqdm import tqdm
 from datetime import datetime, date
 # %%--------------------------------------------Read File-------------------------------
 Florence=r'C:\Users\af_sh\PycharmProjects\BHEModel2.0\resources\meteo data\Florence ERA.csv'
 Munich=r'C:\Users\af_sh\PycharmProjects\BHEModel2.0\resources\meteo data\Munich ERA.csv'
 Cheyenne=r'C:\Users\af_sh\PycharmProjects\BHEModel2.0\resources\meteo data\Cheyenne ERA.csv'
 Riyadh=r'C:\Users\af_sh\PycharmProjects\BHEModel2.0\resources\meteo data\Riyadh ERA.csv'
+
 def Sort_data(City):
+
     Ambient_Temperature = pd.read_csv(City, usecols=['time', 'temperature'])
     Ambient_Temperature['time'] = pd.to_datetime(Ambient_Temperature['time'])
     Monthly_df = \
@@ -31,18 +34,24 @@ def Sort_data(City):
     return Mean_T_amb_list  #annual
 
 def Sort_by_month(City):
+
     monthly_av_list = list()
     b=Sort_data(City)
+
     for o in range(0, len(b), 24):
+
         monthly_av_list.append(b[o:o + 24])
-    monthly_av_list
 
     return monthly_av_list
+
 def plot_T_mean(City):
+
     h = linspace(0, 23, 24)
     for z in range(0, 12):
+
         month = Sort_by_month(City)[z]
         plt.plot(h, month)
+
     plt.ylabel("T_amb")
     plt.xlabel("hour")
     plt.legend((
@@ -53,6 +62,7 @@ def plot_T_mean(City):
         'Oct', 'Nov', 'Dec'),
 
         loc='upper right')
+
     plt.title("hourly T_amb Variation over year 2018")
     plt.show()
 
@@ -60,27 +70,41 @@ def plot_T_mean(City):
 
 # %%-----------------------------------BH input/condenser out point setup-----------------------------------------------------
 def Condenser_T(City):
+
     T_Cond_annual=list()
     delta_T_appr=5
+
     annual_T_list=Sort_data(City)
+
     for s in annual_T_list:
+
         T_mean=float(s)+delta_T_appr
         T_Cond_annual.append(T_mean)
+
     return T_Cond_annual
 
 # %%-
 def Condenser_P(City):
+
     AC_pressure = list()
     C_point = PlantThermoPoint(["Carbon Dioxide"], [1])
     T_annual = Condenser_T(City)
+
     for u in T_annual:
+
         tmp_point = C_point.duplicate()
         tmp_point.set_variable("T", u)
+
         if u < 30:
+
             tmp_point.set_variable("Q", 0)
+
         else:
+
             tmp_point.set_variable("rho", 700)
+
         AC_pressure.append(tmp_point.get_variable("P"))
+
     return AC_pressure
 
 # %%-
@@ -89,12 +113,18 @@ def BH_in_points(City):
     BH_in_point = list()
     BH_point = PlantThermoPoint(["Carbon Dioxide"], [1])
     T_annual=Condenser_T(City)
+
     for ele in T_annual:
+
         tmp_point = BH_point.duplicate()
         tmp_point.set_variable("T", ele)
+
         if ele < 30:
+
             tmp_point.set_variable("Q", 0)
+
         else:
+
             tmp_point.set_variable("rho", 700)
 
         tmp_point.get_variable("P")
@@ -106,14 +136,19 @@ def BH_in_points(City):
 
 # %%-
 def Turbine_in_points(City):
+
     h_in=list()
     s_in=list()
     BH_out_points=list()
+
     dz_well = 1500  # [m] Depth of the reservoir                    #BH output T and P
     T_rock = 125  # [°C] Temperature of the reservoir
+
     BH_points=PlantThermoPoint(["Carbon Dioxide"], [1])
     BH_in_point=BH_in_points(City)
+
     for element in BH_in_point:
+
         bhe_in = SimplifiedBHE(
 
         input_thermo_point=element,
@@ -122,18 +157,21 @@ def Turbine_in_points(City):
         )
 
         bhe_in.update()
+
         output_condition = bhe_in.points[-1]
         h_in.append(output_condition.get_variable("h"))
         s_in.append(output_condition.get_variable("s"))
-        bhe_in.points[-1].copy_state_to(BH_points)
-        BH_out_points.append(BH_points)
+
+        tmp_point = bhe_in.points[-1].duplicate()
+        BH_out_points.append(tmp_point)
+
     print("h_out_BH",h_in)
     print("s_out_BH", s_in)
     return [BH_out_points,s_in,h_in]
 
 # %%-
-
 def Turbine_out_points(City):
+
     Turbine_out_r_points=list()
     eta=0.7
     T_out=list
@@ -144,21 +182,26 @@ def Turbine_out_points(City):
     h_in=Turbine_in_points(City)[2]
     P_out=Condenser_P(City)
     TO_point=PlantThermoPoint(["Carbon Dioxide"], [1])
+
     for iso in range(len(P_out)):
+
         tmp_point = TO_point.duplicate()
         a=s_in[iso]
         b=P_out[iso]
         tmp_point.set_variable("s",a )
         tmp_point.set_variable("P",b)
         h_out_iso.append(tmp_point.get_variable("h"))
+
     for i in range(len(P_out)):
+
         e=h_in[i]
         f=h_out_iso[i]
-        x =  e- (eta * (e - f))
+        x =  e-(eta * (e - f))
         h_out_r.append(x)
 
 
     for tor in range(len(P_out)):   #setting real output
+
         tmp_point = TO_point.duplicate()
         c=h_out_r[tor]
         d=P_out[tor]
@@ -177,16 +220,24 @@ def Turbine_Power(City):
     dh=list()
     Turbine_Power=list()
     Turbine_des_m_dot = 10
-    for i in range(len(Turbine_out_points(City)[1])):
+
+    pbar = tqdm(desc="Calculation", total=len(b))
+    turbine = TurbineOD(input_point=g, output_point=u)
+
+    for i in range(len(b)):
         g=a[i]-b[i]
 
         dh.append(g)
-        #turbine = TurbineOD(input_point=g, output_point=u)
-        #turbine.update_input_output_points(a[i], b[i])
-        #turbine.evaluate_design()
-        #turbine.update_off_design_flow_rate()
-        Turbine_Power.append(float(dh[i])*Turbine_des_m_dot)
+
+        turbine.update_input_output_points(a[i], b[i])
+        turbine.update_off_design_flow_rate()
+
+        Turbine_Power.append(turbine.power)
+        pbar.update(1)
+
+    pbar.close()
     return Turbine_Power
+
 print("Pow",Turbine_Power(Munich))
  # %%-
 
---
+++ calculation/2022-11-28 - Shaila Folder/Off_Design/off-design trial.py
@@ -0,0 +1,41 @@
+from main_code.simplified_well.simplified_well import SimplifiedBHE
+from main_code.support.abstract_plant_thermo_point import PlantThermoPoint
+from main_code.off_design_model.turbine_off_design import TurbineOD
+from pathlib import Path
+import pandas as pd
+import numpy as np
+from numpy import linspace
+from pandas import Series,DataFrame
+import matplotlib.pyplot as plt
+import calendar
+from tqdm import tqdm
+from datetime import datetime, date
+
+t_des = 20
+tmp_point = BH_point.duplicate()
+tmp_point.set_variable("T", ele)
+
+if ele < 30:
+
+    tmp_point.set_variable("Q", 0)
+
+else:
+
+    tmp_point.set_variable("rho", 700)
+
+tmp_point.get_variable("P")
+BH_in_point.append(tmp_point)
+
+turbine = TurbineOD(input_point=input_point, output_point=output_point)
+
+temperature_list = [15, 16, 17, 20, ...] #this should czame from the ambient data
+
+for T in range(len(temperature_list)):
+
+    # evaluate input and outpu of the turbine
+
+    turbine.update_input_output_points(new_input_point, new_output_point)
+    turbine.update_off_design_flow_rate()
+
+    Turbine_Power.append(turbine.power)
+    pbar.update(1)
