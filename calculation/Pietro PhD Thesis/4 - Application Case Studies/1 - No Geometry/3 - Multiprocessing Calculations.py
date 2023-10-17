d98dfeead68ba52725004d6b9e204b9f948f8fbe
Pietro Ungar <pietro.ungar@unifi.it>
2023-10-17 15:32:45 +0200

    First Multiprocessing Completed

--- calculation/Pietro PhD Thesis/4 - Application Case Studies/1 - No Geometry/3 - Multiprocessing Calculations.py
+++ calculation/Pietro PhD Thesis/4 - Application Case Studies/1 - No Geometry/3 - Multiprocessing Calculations.py
@@ -3,8 +3,10 @@ from main_code.power_plants.base_surface_power_plant import BaseSurfacePlant
 from main_code.support.abstract_plant_thermo_point import PlantThermoPoint
 from main_code.constants import CALCULATION_FOLDER, os
 from scipy.optimize import minimize
+import concurrent.futures
 import multiprocessing
 import numpy as np
+import math
 import time
 import gc
 
@@ -12,18 +14,21 @@ result_folder = os.path.join(
 
     CALCULATION_FOLDER, "Pietro PhD Thesis",
     "4 - Application Case Studies", "1 - No Geometry",
-    "res", "support"
+    "res"
 
 )
 
+support_folder = os.path.join(result_folder, "support")
+
 base_filename = "res_{i}_{j}.npy"
+reporting_points = [0.05, 0.25, 0.5, 0.75, 1]
 
-n_temp = 3
-t_ambs = np.linspace(10, 50, n_temp)
+t_ambs = [10, 50]
+n_temp = len(t_ambs)
 
-n_points = 25
-depths_list = np.logspace(2, 4, n_points)
-grads_list = np.logspace(1, 2, n_points)
+n_points = 75
+depths_list = np.logspace(np.log10(250), np.log10(5000), n_points)
+grads_list = np.logspace(np.log10(25), 2, n_points)
 depths, grads = np.meshgrid(depths_list, grads_list, indexing="ij")
 
 fluid = "Carbon Dioxide"
@@ -31,6 +36,7 @@ bhe_in = PlantThermoPoint([fluid], [1])
 bhe_in.set_variable("T", bhe_in.RPHandler.TC)
 bhe_in.set_variable("P", bhe_in.RPHandler.PC)
 rho_crit = bhe_in.get_variable("rho")
+
 
 def update_values(curr_bhe, t_amb, p_in):
 
@@ -43,9 +49,16 @@ def update_values(curr_bhe, t_amb, p_in):
 
     return new_base_surface
 
+
 def evaluate_part(i, j):
 
-    print(f'{i}-{j} Started')
+    rep_pos = 0
+    curr_value = i * n_points + j + 1
+    max_digits = math.ceil(np.log10(n_points*n_temp))
+    current_name = "{{:{}d}} of {{}}".format(max_digits).format(curr_value, n_points*n_temp)
+
+    print("{} - Started".format(current_name))
+
     sub_start = time.time()
 
     sub_results = np.empty([2 * 3, n_points])
@@ -97,30 +110,52 @@ def evaluate_part(i, j):
             sub_results[n, k] = opt_res_x
             sub_results[n + 3, k] = opt_res
 
-    filename = os.path.join(result_folder, base_filename.format(i=i, j=j))
+        gc.collect()
+
+        if k / n_points > reporting_points[rep_pos]:
+
+            time_elapsed = time.time() - sub_start
+            time_remaining = time_elapsed * (1 - reporting_points[rep_pos]) / reporting_points[rep_pos]
+
+            print("{} - {:.0f}% done! -> {:.0f}s ({:.0f}min) elapsed, {:.0f}s ({:.0f}min) to-go".format(
+
+                current_name, reporting_points[rep_pos]*100,
+                time_elapsed, time_elapsed/60,
+                time_remaining, time_remaining/60
+
+            ))
+
+            rep_pos += 1
+
+    filename = os.path.join(support_folder, base_filename.format(i=i, j=j))
     np.save(filename, sub_results)
 
-    gc.collect()
-    print(f'{i}-{j} Done - {time.time() - sub_start}s')
+    time_elapsed = time.time() - sub_start
+
+    print("{} - Done! -> {:.0f}s ({:.0f}min) elapsed".format(current_name, time_elapsed, time_elapsed / 60))
+
+    return time_elapsed
 
 
 if __name__ == '__main__':
 
     print("Calculation Started!!")
 
-    processes = list()
-    for i in range(n_temp):
+    with concurrent.futures.ProcessPoolExecutor() as executor:
 
-        for j in range(n_points):
+        future_list = list()
 
-            p = multiprocessing.Process(target=evaluate_part, args=[i, j])
-            p.start()
+        for i in range(n_temp):
 
-            processes.append(p)
+            for j in range(n_points):
 
-    for process in processes:
+                time.sleep(0.5)
+                future_list.append(executor.submit(evaluate_part, i, j))
 
-        process.join()
+        elapsed_time_list = list()
+        for f in concurrent.futures.as_completed(future_list):
+
+            elapsed_time_list.append(f.result())
 
     print("Calculation Completed!!")
     print("Saving Results ...")
@@ -134,13 +169,26 @@ if __name__ == '__main__':
 
         for j in range(n_points):
 
-            filename = os.path.join(result_folder, base_filename.format(i=i, j=j))
+            filename = os.path.join(support_folder, base_filename.format(i=i, j=j))
 
             if os.path.exists(filename):
 
                 results[:, i, j, :] = np.load(filename)
-                os.remove(filename)
+                # os.remove(filename)
 
-    print(results)
-    filename = os.path.join(result_folder, "Optimization Result")
+    filename = os.path.join(result_folder, "opt_results.npy")
     np.save(filename, results)
+
+    filename = os.path.join(result_folder, "depths_mesh.npy")
+    np.save(filename, depths)
+
+    filename = os.path.join(result_folder, "grads_mesh.npy")
+    np.save(filename, grads)
+
+    filename = os.path.join(result_folder, "t_ambs.npy")
+    np.save(filename, t_ambs)
+
+    filename = os.path.join(result_folder, "elapsed_times.npy")
+    np.save(filename, np.array(elapsed_time_list))
+
+    print("Calculation Completed!!!")
--- calculation/Pietro PhD Thesis/4 - Application Case Studies/1 - No Geometry/4 - Plot Results.py
+++ calculation/Pietro PhD Thesis/4 - Application Case Studies/1 - No Geometry/4 - Plot Results.py
@@ -9,26 +9,31 @@ result_folder = os.path.join(
 
     CALCULATION_FOLDER, "Pietro PhD Thesis",
     "4 - Application Case Studies", "1 - No Geometry",
-    "res", "support"
+    "res"
 
 )
-filename = os.path.join(result_folder, "Optimization Result.npy")
+filename = os.path.join(result_folder, "opt_results.npy")
 results = np.load(filename)
-res_shape = results.shape
 
+filename = os.path.join(result_folder, "depths_mesh.npy")
+depths = np.load(filename)
+
+filename = os.path.join(result_folder, "grads_mesh.npy")
+grads = np.load(filename)
+
+filename = os.path.join(result_folder, "t_ambs.npy")
+t_ambs = np.load(filename)
+
+res_shape = results.shape
 n_points = res_shape[-1]
 n_temp = res_shape[1]
 
-depths_list = np.logspace(2, 4, n_points)
-grads_list = np.logspace(1, 2, n_points)
-depths, grads = np.meshgrid(depths_list, grads_list, indexing="ij")
-
-error_max = np.where(results[5] > 1)
-error_zeros = np.where(results[5] <= 0.2)
-
-for i in range(6):
-    results[i][error_max] = np.nan
-    results[i][error_zeros] = np.nan
+# error_max = np.where(results[5] > 1)
+# error_zeros = np.where(results[5] <= 0.2)
+#
+# for i in range(6):
+#     results[i][error_max] = np.nan
+#     results[i][error_zeros] = np.nan
 
 
 # %%-------------------------------------   PLOT LINES                          -------------------------------------> #
@@ -37,14 +42,14 @@ fig.set_size_inches(10, 5)
 cmap = plt.get_cmap('viridis')
 norm = plt.Normalize(0, 1)
 
-i = 2
+i = 1
 for j in range(n_points):
 
     color = cmap(norm(j / (n_points - 1)))
 
     for k in range(len(axs)):
 
-        axs[k].plot(depths_list, results[k, i, :, j], color=color, zorder=1)
+        axs[k].plot(depths[:, j], results[k, i, :, j], color=color, zorder=1)
         axs[k].set_xscale("log")
 
         if k < len(axs) - 1:
